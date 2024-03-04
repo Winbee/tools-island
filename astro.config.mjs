4b02200afb05c5688fbd4f35262755ed456a7c6a
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2024-03-04 21:49:35 +0100

    add deploy github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://winbee.github.com",
+  base: "/tools-island",
   integrations: [
     starlight({
       title: "Tools island",
