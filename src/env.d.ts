3365842d28550d1d5e1a6a3b41a0b17ed7950e46
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2024-03-04 22:10:47 +0100

    Fix window global env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  StarlightThemeProvider: {
+    updatePickers(theme?: string): void;
+  };
+}
