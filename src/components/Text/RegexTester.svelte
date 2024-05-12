7f3195b30a47d4469bc48bae3b2a5b83871e6d10
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2024-05-12 05:29:05 +0200

    Add flags in regex tester

--- src/components/Text/RegexTester.svelte
+++ src/components/Text/RegexTester.svelte
@@ -3,6 +3,7 @@
   import FormWrapper from "../FormWrapper.svelte";
 
   let regex = "t";
+  let flags = "g";
   let testString = "test";
   let computedRegex = new RegExp("");
   let invalidRegex = false;
@@ -17,11 +18,13 @@
     try {
       invalidRegex = false;
       // We need to recreate the regex to have the correct index
-      regexTest = new RegExp(regex, "g").test(testString);
-      regexExec = new RegExp(regex, "g").exec(testString);
-      computedRegex = new RegExp(regex, "g");
+      regexTest = new RegExp(regex, flags).test(testString);
+      regexExec = new RegExp(regex, flags).exec(testString);
+      computedRegex = new RegExp(regex, flags);
       stringMatch = testString.match(computedRegex);
-      stringMatchAll = testString.matchAll(computedRegex);
+      stringMatchAll = flags.includes("g")
+        ? testString.matchAll(computedRegex)
+        : undefined;
       stringSearch = testString.search(computedRegex);
       stringSplit = testString.split(computedRegex);
     } catch (error) {
@@ -47,6 +50,17 @@
   </div>
 
   <div>
+    <Label for="flags">Flags</Label>
+    <Input
+      type="text"
+      id="flags"
+      class="mt-2"
+      bind:value={flags}
+      on:input={runRegex}
+    />
+  </div>
+
+  <div>
     <Label for="input" class="mb-2">Text</Label>
     <Textarea
       id="input"
@@ -64,50 +78,56 @@
       <div
         class="grid grid-cols-2 rounded-lg border border-solid border-gray-200 dark:border-gray-600 overflow-hidden"
       >
-        <div class="bg-gray-50 dark:bg-gray-700 p-1 font-medium">Method</div>
-        <div class="bg-gray-50 dark:bg-gray-700 p-1 font-medium">Result</div>
-        <div class="p-1">String.match()</div>
-        <div class="p-1">
+        <div class="bg-gray-50 dark:bg-gray-700 px-2 py-1 font-medium">
+          Method
+        </div>
+        <div class="bg-gray-50 dark:bg-gray-700 px-2 py-1 font-medium">
+          Result
+        </div>
+        <div class="px-2 py-1">String.match()</div>
+        <div class="px-2 py-1">
           {JSON.stringify(stringMatch, null, " ")}
         </div>
-        <div class="p-1">String.matchAll()</div>
-        <div class="p-1">
-          {stringMatchAll === null
+        <div class="px-2 py-1">String.matchAll()</div>
+        <div class="px-2 py-1">
+          {stringMatchAll == null
             ? null
             : JSON.stringify([...stringMatchAll], null, " ")}
         </div>
-        <div class="p-1">String.search()</div>
-        <div class="p-1">
+        <div class="px-2 py-1">String.search()</div>
+        <div class="px-2 py-1">
           {JSON.stringify(stringSearch, null, " ")}
         </div>
-        <div class="p-1">String.split()</div>
-        <div class="p-1">
+        <div class="px-2 py-1">String.split()</div>
+        <div class="px-2 py-1">
           {JSON.stringify(stringSplit, null, " ")}
         </div>
-        <div class="p-1">Regex.test()</div>
-        <div class="p-1">{regexTest}</div>
-        <div class="p-1">Regex.exec()</div>
+        <div class="px-2 py-1">Regex.test()</div>
+        <div class="px-2 py-1">{regexTest}</div>
+        <div class="px-2 py-1">Regex.exec()</div>
         <div class="grid grid-cols-2">
           {#if regexExec === null}
-            <div class="p-1">{regexExec}</div>
+            <div class="px-2 py-1">{regexExec}</div>
           {:else}
-            <div class="bg-gray-50 dark:bg-gray-700 p-1 font-medium">
+            <div class="bg-gray-50 dark:bg-gray-700 px-2 py-1 font-medium">
               Property
             </div>
-            <div class="bg-gray-50 dark:bg-gray-700 p-1 font-medium">Value</div>
-            <div class="p-1">"raw indexes"</div>
-            <div class="p-1">
+            <div class="bg-gray-50 dark:bg-gray-700 px-2 py-1 font-medium">
+              Value
+            </div>
+            <div class="px-2 py-1">"raw indexes"</div>
+            <div class="px-2 py-1">
               <span>{JSON.stringify(regexExec, null, " ")}</span>
             </div>
-            <div class="p-1">groups</div>
-            <div class="p-1">
+            <div class="px-2 py-1">groups</div>
+            <div class="px-2 py-1">
               {JSON.stringify(regexExec.groups, null, " ")}
             </div>
-            <div class="p-1">index</div>
-            <div class="p-1">
+            <div class="px-2 py-1">index</div>
+            <div class="px-2 py-1">
               {regexExec.index}
             </div>
-            <div class="p-1">input</div>
+            <div class="px-2 py-1">input</div>
             <div>
               {JSON.stringify(regexExec.input, null, " ")}
             </div>
